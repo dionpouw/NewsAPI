fbb4a477ace90ec1763e9167e5c230e041fe7715
dionpouw <aldion.sumampouw@gmail.com>
2022-07-16 12:01:22 +0800

    created Adapter for News

---
+++ app/src/main/java/com/jeflette/newsapi/ui/adapter/NewsAdapter.kt
@@ -0,0 +1,24 @@
+package com.jeflette.newsapi.ui.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class NewsAdapter : RecyclerView.Adapter<NewsAdapter.ViewHolder>() {
+
+    inner class ViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(parent) {
+
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
