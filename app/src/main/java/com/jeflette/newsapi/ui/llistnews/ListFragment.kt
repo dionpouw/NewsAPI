503540553b7b699a7f2e80c727865129115f4d11
dionpouw <aldion.sumampouw@gmail.com>
2022-07-21 22:16:09 +0800

    List Fragment fix

--- app/src/main/java/com/jeflette/newsapi/ui/llistnews/ListFragment.kt
+++ app/src/main/java/com/jeflette/newsapi/ui/llistnews/ListFragment.kt
@@ -1,27 +1,52 @@
 package com.jeflette.newsapi.ui.llistnews
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.jeflette.newsapi.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.jeflette.newsapi.data.remote.response.Articles
+import com.jeflette.newsapi.databinding.FragmentListBinding
 import com.jeflette.newsapi.ui.MainViewModel
+import com.jeflette.newsapi.ui.adapter.NewsAdapter
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ListFragment : Fragment() {
 
-    private lateinit var viewModel: MainViewModel
+    private val viewModel: MainViewModel by viewModels()
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_list, container, false)
+    ): View {
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-       // TODO: Use the ViewModel
+
+        viewModel.getNews()
+        val newsAdapter = NewsAdapter()
+        binding.rvNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+        }
+
+        viewModel.news.observe(viewLifecycleOwner) {
+            newsAdapter.setList(it.articles as List<Articles>)
+        }
+        // TODO: Use the ViewModel
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
