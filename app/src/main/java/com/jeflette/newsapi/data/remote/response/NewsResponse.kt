089a7cd2bdbdfe99de432d8dd5c8c1bbc3fcedb4
dionpouw <aldion.sumampouw@gmail.com>
2022-07-21 22:16:47 +0800

    News Response

--- app/src/main/java/com/jeflette/newsapi/data/remote/response/NewsResponse.kt
+++ app/src/main/java/com/jeflette/newsapi/data/remote/response/NewsResponse.kt
@@ -2,7 +2,6 @@ package com.jeflette.newsapi.data.remote.response
 
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
-import com.jeflette.newsapi.data.local.entity.Articles
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
@@ -16,4 +15,42 @@ data class NewsResponse(
 
     @field:SerializedName("status")
     val status: String? = null
+) : Parcelable
+
+@Parcelize
+data class Articles(
+
+    @field:SerializedName("publishedAt")
+    val publishedAt: String? = null,
+
+    @field:SerializedName("author")
+    val author: String? = null,
+
+    @field:SerializedName("urlToImage")
+    val urlToImage: String? = null,
+
+    @field:SerializedName("description")
+    val description: String? = null,
+
+    @field:SerializedName("source")
+    val source: Source? = null,
+
+    @field:SerializedName("title")
+    val title: String? = null,
+
+    @field:SerializedName("url")
+    val url: String? = null,
+
+    @field:SerializedName("content")
+    val content: String? = null
+) : Parcelable
+
+@Parcelize
+data class Source(
+
+    @field:SerializedName("name")
+    val name: String? = null,
+
+    @field:SerializedName("id")
+    val id: String? = null
 ) : Parcelable
