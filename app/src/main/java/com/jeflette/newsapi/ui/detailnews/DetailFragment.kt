535a3ea0e83b3355399674a6840939cc64c4efb9
dionpouw <aldion.sumampouw@gmail.com>
2022-07-21 22:15:37 +0800

    add utils to change future date to Locale date

--- app/src/main/java/com/jeflette/newsapi/ui/detailnews/DetailFragment.kt
+++ app/src/main/java/com/jeflette/newsapi/ui/detailnews/DetailFragment.kt
@@ -5,20 +5,30 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.jeflette.newsapi.R
+import com.jeflette.newsapi.databinding.FragmentDetailBinding
+import com.jeflette.newsapi.databinding.FragmentListBinding
 
 class DetailFragment : Fragment() {
+
+    private val args: DetailFragmentArgs by navArgs()
+    private var _binding: FragmentDetailBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+    ): View {
+        _binding = FragmentDetailBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        // TODO: Use the ViewModel
+        Glide.with(this).load(args.news.urlToImage).into(binding.detailImage)
+        binding.detailTitle.text = args.news.title
+        binding.detailDescription.text = args.news.content
     }
-
 }
