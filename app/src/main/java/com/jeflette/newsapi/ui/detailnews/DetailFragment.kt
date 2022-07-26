6afe83aff644c3f3e12d17c15d14775cfc13ce76
dionpouw <aldion.sumampouw@gmail.com>
2022-07-26 21:39:37 +0800

    Detail fragment and Viewmodel

--- app/src/main/java/com/jeflette/newsapi/ui/detailnews/DetailFragment.kt
+++ app/src/main/java/com/jeflette/newsapi/ui/detailnews/DetailFragment.kt
@@ -5,13 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebChromeClient
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.jeflette.newsapi.databinding.FragmentDetailBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class DetailFragment : Fragment() {
 
     private val args: DetailFragmentArgs by navArgs()
+    private val viewModel: DetailViewModel by viewModels()
     private var _binding: FragmentDetailBinding? = null
     private val binding get() = _binding!!
 
@@ -25,23 +31,47 @@ class DetailFragment : Fragment() {
 
     @Deprecated("Deprecated in Java")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        binding.webViewNews.apply {
-            clearCache(true)
-            webChromeClient = WebChromeClient()
-            settings.apply {
-                javaScriptEnabled = true
-                useWideViewPort = true
-                domStorageEnabled = true
+
+        val callback = requireActivity()
+            .onBackPressedDispatcher
+            .addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    // Do custom work here
+
+                    // if you want onBackPressed() to be called as normal afterwards
+                    if (isEnabled) {
+                        isEnabled = false
+                        requireActivity().onBackPressed()
+                    }
+                }
             }
-            args.news.url?.let {
-                loadUrl(it)
+            )
+
+        super.onActivityCreated(savedInstanceState)
+        binding.apply {
+            webViewNews.apply {
+                clearCache(true)
+                webChromeClient = WebChromeClient()
+                settings.apply {
+                    javaScriptEnabled = true
+                    useWideViewPort = true
+                    domStorageEnabled = true
+                }
+                args.articles?.url?.let {
+                    loadUrl(it)
+                }
+            }
+            fabAddBookmarked.setOnClickListener {
+                args.articles?.let { article -> viewModel.updateArticles(article, true) }
             }
         }
     }
+
 
     override fun onDetach() {
         super.onDetach()
         _binding = null
     }
+
+
 }
