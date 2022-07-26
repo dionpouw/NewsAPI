95f2236c4fc789f8a7e2637a1b2352a0070229f3
dionpouw <aldion.sumampouw@gmail.com>
2022-07-26 21:39:24 +0800

    News adapter fix

--- app/src/main/java/com/jeflette/newsapi/ui/adapter/NewsAdapter.kt
+++ app/src/main/java/com/jeflette/newsapi/ui/adapter/NewsAdapter.kt
@@ -3,24 +3,21 @@ package com.jeflette.newsapi.ui.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.navigation.Navigation.findNavController
-import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.jeflette.newsapi.R
-import com.jeflette.newsapi.data.entity.News
-import com.jeflette.newsapi.data.remote.response.Articles
+import com.jeflette.newsapi.data.entity.Articles
 import com.jeflette.newsapi.databinding.NewsItemBinding
+import com.jeflette.newsapi.ui.bookmark.BookmarkFragmentDirections
 import com.jeflette.newsapi.ui.llistnews.ListFragmentDirections
 import com.jeflette.newsapi.util.withDateFormat
 
 class NewsAdapter() : RecyclerView.Adapter<NewsAdapter.ViewHolder>() {
 
-    private val results: MutableList<Articles?>? = ArrayList()
-    fun setList(result: List<Articles?>?) {
-        results?.clear()
-        if (result != null) {
-            results?.addAll(result)
-        }
+    private val results: MutableList<Articles> = ArrayList()
+    fun setList(result: List<Articles>) {
+        results.clear()
+        results.addAll(result)
         notifyDataSetChanged()
     }
 
@@ -43,22 +40,23 @@ class NewsAdapter() : RecyclerView.Adapter<NewsAdapter.ViewHolder>() {
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        results?.get(position)?.let { holder.binding(it) }
+        holder.binding(results[position])
         holder.itemView.setOnClickListener {
-            val action =
+            val actionListFragmentToDetailFragment =
                 ListFragmentDirections.actionListFragmentToDetailFragment(
-                    results?.get(position) ?: Articles()
+                    results[position]
                 )
-            findNavController(it).navigate(action)
+
+            val actionBookmarkFragmentToDetailFragment =
+                BookmarkFragmentDirections.actionBookmarkFragmentToDetailFragment(
+                    results[position]
+                )
+            findNavController(it).navigate(actionListFragmentToDetailFragment)
+            if (findNavController(it).currentDestination ==) {
+
+            }
         }
     }
 
-    override fun getItemCount(): Int = results?.size ?: 0
-
-    class NewsComparator : DiffUtil.ItemCallback<News>() {
-        override fun areItemsTheSame(oldItem: News, newItem: News): Boolean =
-            oldItem.title == newItem.title
-
-        override fun areContentsTheSame(oldItem: News, newItem: News): Boolean = oldItem == newItem
-    }
+    override fun getItemCount(): Int = results.size ?: 0
 }
