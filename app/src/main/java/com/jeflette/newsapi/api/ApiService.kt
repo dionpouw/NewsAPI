7f7eb36860aef01c6e89091384641e9ccffbed74
dionpouw <aldion.sumampouw@gmail.com>
2022-07-26 21:36:10 +0800

    api service for retrofit

--- app/src/main/java/com/jeflette/newsapi/api/ApiService.kt
+++ app/src/main/java/com/jeflette/newsapi/api/ApiService.kt
@@ -1,6 +1,6 @@
 package com.jeflette.newsapi.api
 
-import com.jeflette.newsapi.data.remote.response.NewsResponse
+import com.jeflette.newsapi.data.remote.NewsResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
